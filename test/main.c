65d28b907f4078070a5778e6e1b4d5268d168b15
Bivas Kumar <thetrotfreak@yahoo.com>
2023-12-16 11:58:16 +0530

    Add ESE-II submitted code
    Unmodified but with a Makefile

---
+++ test/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "patient.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int front = -1;
+int rear = -1;
+
+int main(int argc, char *argv[]) {
+	Patient p1 = {
+			.id=1,
+			.age=12,
+			.severity=1
+	};
+	Patient p2 = {
+			.id=2,
+			.age=22,
+			.severity=3
+	};
+	Patient p3 = {
+			.id=3,
+			.age=33,
+			.severity=2
+	};
+	Patient p[SIZE];
+	
+	enqueue(&p[0], p1);
+	enqueue(&p[0], p2);
+	enqueue(&p[0], p3);
+	printSevere(&p[0]);
+	stats(&p[0]);
+	traverse(&p[0]);
+	return 0;
+}
---
+++ test/patient.h
@@ -0,0 +1,30 @@
+#ifndef _PATIENT_H
+#define _PATIENT_H
+
+#define SIZE 128
+
+typedef enum t_Severity_Level_of_Illness{
+	L1, // 0
+	L2, // 1
+	L3, // 2
+	L4, // 3
+	L5  // 4
+} Severity;
+
+typedef struct t_Patient {
+	unsigned int id;
+	unsigned short age;
+	int severity;
+} Patient;
+
+extern int front;
+extern int rear;
+
+void printPatient(Patient *);
+void enqueue(Patient *, Patient);
+void traverse(Patient *);
+void printSevere(Patient *);
+void stats(Patient *);
+int cmp(const void *p1, const void *p2);
+
+#endif // _PATIENT_H
