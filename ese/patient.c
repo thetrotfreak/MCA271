7c333b721e936778b760dcd03c0fa5c315db1ec6
Bivas Kumar <thetrotfreak@yahoo.com>
2023-12-19 22:25:16 +0530

    clang format

--- ese/patient.c
+++ ese/patient.c
@@ -3,114 +3,157 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printPatient(Patient *p){
-	if(p!=NULL){
-		printf("==========\n");
-		printf("ID       : %u\n", p->id);
-		printf("Age      : %hd\n", p->age);
-		printf("Severity : %d\n", p->severity);
-		printf("==========\n");
-	}
+void
+printPatient (Patient *p)
+{
+  if (p != NULL)
+    {
+      printf ("==========\n");
+      printf ("ID       : %u\n", p->id);
+      printf ("Age      : %hd\n", p->age);
+      printf ("Severity : %d\n", p->severity);
+      printf ("==========\n");
+    }
 }
 
-int validPatient(Patient *p){
-	if(p->severity>0 && p->severity<6){
-		return 1;
-	}else{
-		fprintf(stderr, "Invalid Illness Severity Level\n");
-		return 0;
-	}
+int
+validPatient (Patient *p)
+{
+  if (p->severity > 0 && p->severity < 6)
+    {
+      return 1;
+    }
+  else
+    {
+      fprintf (stderr, "Invalid Illness Severity Level\n");
+      return 0;
+    }
 }
-void enqueue(Patient *a, Patient p){
-	if(front == -1 & rear == -1){
-		if(validPatient(&p)){
-			front++;
-			rear++;
-			a[rear] = p;
-		}
-	}else{
-		if(validPatient(&p)){
-			a[++rear] = p;
-		}
-	qsort(&a[0], rear+1, sizeof(Patient), cmp);
-	}
-}
-
-void traverse(Patient *a){
-	if(front == -1 && rear == -1){
-		fprintf(stderr, "UNDERFLOW\n");
-	}else if(front == 0 && rear == 0){
-		printPatient(&a[rear]);
-	}else{
-		for(int i=front; i<=rear; i++){
-			printPatient(&a[i]);
-		}
-	}
-}
-
-void printSevere(Patient *a){
-	if(front == -1 && rear == -1){
-		fprintf(stderr, "UNDERFLOW\n");
-	}else{
-		printf("\n===========================================\n");
-		printf("                   Severe                      ");
-		printf("\n===========================================\n");
-		for(int i=front; i<=rear; i++){
-			if(a[i].severity == 1){
-				printPatient(&a[rear]);
-			}
-		}		
-	}
+void
+enqueue (Patient *a, Patient p)
+{
+  if (front == -1 & rear == -1)
+    {
+      if (validPatient (&p))
+        {
+          front++;
+          rear++;
+          a[rear] = p;
+        }
+    }
+  else
+    {
+      if (validPatient (&p))
+        {
+          a[++rear] = p;
+        }
+      qsort (&a[0], rear + 1, sizeof (Patient), cmp);
+    }
 }
 
-void stats(Patient *a){
-	if(front == -1 && rear == -1){
-		fprintf(stderr, "UNDERFLOW\n");
-	}else{
-		int l1=0;
-		int l2=0;
-		int l3=0;
-		int l4=0;
-		int l5=0;
-		for(int i=front; i<=rear; i++){
-			switch(a[i].severity){
-				case 1:
-					l1++;
-					break;
-				case 2:
-					l2++;
-					break;
-				case 3:
-					l3++;
-					break;
-				case 4:
-					l4++;
-					break;
-				case 5:
-					l5++;
-					break;
-			}
-		}
-		printf("\n===========================================\n");
-		printf("                 Statistics                    ");
-		printf("\n===========================================\n");
-		printf("Frequency of Patients with Severity Level 1 : %d\n", l1);
-		printf("Frequency of Patients with Severity Level 2 : %d\n", l2);
-		printf("Frequency of Patients with Severity Level 3 : %d\n", l3);
-		printf("Frequency of Patients with Severity Level 4 : %d\n", l4);
-		printf("Frequency of Patients with Severity Level 5 : %d\n", l5);
-	}	
+void
+traverse (Patient *a)
+{
+  if (front == -1 && rear == -1)
+    {
+      fprintf (stderr, "UNDERFLOW\n");
+    }
+  else if (front == 0 && rear == 0)
+    {
+      printPatient (&a[rear]);
+    }
+  else
+    {
+      for (int i = front; i <= rear; i++)
+        {
+          printPatient (&a[i]);
+        }
+    }
 }
 
-int cmp(const void *p1, const void *p2){
-	Patient *p = (Patient *)p1;
-	Patient *q = (Patient *)p2;
-	if(p->severity < q->severity){
-		return -1;
-	}else if(p->severity > q->severity){
-		return 1;
-	}else{
-		return 0;
-	}
+void
+printSevere (Patient *a)
+{
+  if (front == -1 && rear == -1)
+    {
+      fprintf (stderr, "UNDERFLOW\n");
+    }
+  else
+    {
+      printf ("\n===========================================\n");
+      printf ("                   Severe                      ");
+      printf ("\n===========================================\n");
+      for (int i = front; i <= rear; i++)
+        {
+          if (a[i].severity == 1)
+            {
+              printPatient (&a[rear]);
+            }
+        }
+    }
 }
 
+void
+stats (Patient *a)
+{
+  if (front == -1 && rear == -1)
+    {
+      fprintf (stderr, "UNDERFLOW\n");
+    }
+  else
+    {
+      int l1 = 0;
+      int l2 = 0;
+      int l3 = 0;
+      int l4 = 0;
+      int l5 = 0;
+      for (int i = front; i <= rear; i++)
+        {
+          switch (a[i].severity)
+            {
+            case 1:
+              l1++;
+              break;
+            case 2:
+              l2++;
+              break;
+            case 3:
+              l3++;
+              break;
+            case 4:
+              l4++;
+              break;
+            case 5:
+              l5++;
+              break;
+            }
+        }
+      printf ("\n===========================================\n");
+      printf ("                 Statistics                    ");
+      printf ("\n===========================================\n");
+      printf ("Frequency of Patients with Severity Level 1 : %d\n", l1);
+      printf ("Frequency of Patients with Severity Level 2 : %d\n", l2);
+      printf ("Frequency of Patients with Severity Level 3 : %d\n", l3);
+      printf ("Frequency of Patients with Severity Level 4 : %d\n", l4);
+      printf ("Frequency of Patients with Severity Level 5 : %d\n", l5);
+    }
+}
+
+int
+cmp (const void *p1, const void *p2)
+{
+  Patient *p = (Patient *)p1;
+  Patient *q = (Patient *)p2;
+  if (p->severity < q->severity)
+    {
+      return -1;
+    }
+  else if (p->severity > q->severity)
+    {
+      return 1;
+    }
+  else
+    {
+      return 0;
+    }
+}
